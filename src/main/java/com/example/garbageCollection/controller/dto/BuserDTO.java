7b3a2fb47607cbd0e7cb0ee51d2c91da17c26262
chenyu <chenyu@iict.ac.cn>
2024-04-02 11:14:23 +0800

    feat: 完成用户功能的注册

---
+++ src/main/java/com/example/garbageCollection/controller/dto/BuserDTO.java
@@ -0,0 +1,18 @@
+package com.example.garbageCollection.controller.dto;
+
+import com.example.garbageCollection.entity.Menu;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BuserDTO {
+    private String username;
+    private String password;
+    private String nickname;
+    private String avatarUrl;
+    private String gender; // new
+    private String introduction; // new
+    private String token; // 返回用户token
+}
+
